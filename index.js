5f4e911d1f179cc5a8ba91a307ede36d7ca08d55
zazu135 <142782991+zazu135@users.noreply.github.com>
2024-04-18 17:23:12 +0400

    added function for flags to appear next to country selected

--- index.js
+++ index.js
@@ -1,51 +1,88 @@
-    function updateTime() {
-    
-    let losAngelesElement = document.querySelector("#los-angeles");
-    if (losAngelesElement) {
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  if (losAngelesElement) {
       let losAngelesDateElement = losAngelesElement.querySelector(".date");
-      let lostAngelesTimeElement = losAngelesElement.querySelector(".time");
+      let losAngelesTimeElement = losAngelesElement.querySelector(".time");
       let losAngelesTime = moment().tz("America/Los_Angeles");
-  
-      losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM	Do YYYY");
-      lostAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]");
-    }
-  
-    let torontoElement = document.querySelector("#toronto");
-    if (torontoElement) {
+
+      losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+      losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]");
+  }
+
+  let torontoElement = document.querySelector("#toronto");
+  if (torontoElement) {
       let torontoDateElement = torontoElement.querySelector(".date");
       let torontoTimeElement = torontoElement.querySelector(".time");
       let torontoTime = moment().tz("Canada/Eastern");
-  
+
       torontoDateElement.innerHTML = torontoTime.format("MMMM Do YYYY");
       torontoTimeElement.innerHTML = torontoTime.format("h:mm:ss [<small>]A[</small>]");
-     }
-    }
+  }
 
-    function updateCity(event) {
-      let cityTimeZone = event.target.value;
-      if (cityTimeZone === "current") {
-        cityTimeZone = moment.tz.guess();
-      }
-      let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-      let cityTime = moment().tz(cityTimeZone);
-      let citiesElement = document.querySelector("#cities");
-      citiesElement.innerHTML = `
-      <div class="city">
-        <div>
-          <h2>${cityName}</h2>
-          <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
-        </div>
-        <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
-        "A"
-        )}</small></div>
-        </div>
-        <a href="index.html">All cities</a>
-        `;
-      }
+  let palestineElement = document.querySelector("#palestine");
+  if (palestineElement) {
+      let palestineDateElement = palestineElement.querySelector(".date");
+      let palestineTimeElement = palestineElement.querySelector(".time");
+      let palestineTime = moment().tz("Asia/Gaza");
+
+      palestineDateElement.innerHTML = palestineTime.format("MMMM Do YYYY");
+      palestineTimeElement.innerHTML = palestineTime.format("h:mm:ss [<small>]A[</small>]");
+  }
+
+  let ethiopiaElement = document.querySelector("#ethiopia");
+  if (ethiopiaElement) {
+      let ethiopiaDateElement = ethiopiaElement.querySelector(".date");
+      let ethiopiaTimeElement = ethiopiaElement.querySelector(".time");
+      let ethiopiaTime = moment().tz("Africa/Addis_Ababa");
+
+      ethiopiaDateElement.innerHTML = ethiopiaTime.format("MMMM Do YYYY");
+      ethiopiaTimeElement.innerHTML = ethiopiaTime.format("h:mm:ss [<small>]A[</small>]");
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+      cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+
+
+  const flagMap = {
+      "Algeria": "🇩🇿",
+      "Barbados": "🇧🇧",
+      "London": "🏴",
+      "New York": "🇺🇸",
+      "Auckland": "🇳🇿",
+      "Palestine": "🇵🇸",
+      "Ethiopia": "🇪🇹",
+      "Los Angeles": "🇺🇸",
+      "Dubai":"🇦🇪",
+      "Amman":"🇯🇴",
+      "Ciaro":"🇪🇬",
       
-    
-    updateTime();
-    setInterval(updateTime, 1000);
-    
-    let citiesSelectElement = document.querySelector("#city");
-    citiesSelectElement.addEventListener("change", updateCity);
+  };
+
+  let flag = flagMap[cityName] ? flagMap[cityName] : "";
+  citiesElement.innerHTML = `
+  <div class="city">
+      <div>
+          <h2>${cityName} ${flag}</h2>
+          <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>
+      <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+  </div>
+  <a href="index.html" style="color: black; font-size: 17px; text-decoration: underline;">All cities</a>
+  `;
+}
+
+
+
+
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
